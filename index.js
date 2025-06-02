02f93730fd086efea5d63c1a205e8ca9d9728074
judy gikuni <judygikuni@gmail.com>
2025-06-02 13:17:33 +0300

    added files for the DOM assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = "#c4c4c4";
+
+const heading = document.getElementById('title');
+
+heading.style.color = "green";
+
+const headingThree = document.getElementById('heading-three');
+const upperCase = headingThree.toUpperCase();
+
+console.log(upperCase)
